e0537c22b90eba6fde72be7c55248282dcdf0fb3
Ilia <tereshchen@sfedu.ru>
2023-08-07 22:54:18 +0300

    Initial commit

---
+++ src/main/java/org/example/ComplexRepository.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public interface ICalculator {
+    ComplexDigit sumComplex(ComplexDigit a, ComplexDigit b);
+//    ComplexDigit multiplyComplex(ComplexDigit a, ComplexDigit b);
+//    ComplexDigit divisionComplex(ComplexDigit a, ComplexDigit b);
+}
